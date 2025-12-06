b9b08a399231855507f4c028d3d1cb8d12ac000e
saharnasri <snasri258@gmail.com>
2025-12-06 23:55:53 +0330

    feat: login page in progressing

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ import LoginForm from "./LoginForm";
 function LoginPage() {
   return (
     <section>
-      <h1 className="font-bold text-lg">LogIn:</h1>
+      <h1 className="font-bold text-lg mb-8">LogIn:</h1>
       <LoginForm />
       <div className="text-gray-500 text-center">
         <div>
--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -2,9 +2,10 @@ import React from "react";
 // interface IBtn extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 function Button({
   children,
+  className,
 }: Readonly<React.ButtonHTMLAttributes<HTMLButtonElement>>) {
   return (
-    <button className="bg-green-500 rounded-md min-w-20 min-h-8">
+    <button className={`bg-green-500 rounded-md min-w-20 min-h-8 ${className}`}>
       {children}
     </button>
   );
