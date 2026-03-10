f287e7b7df1f14f99b757c77f9478fa52ae25e89
saharnasri <snasri258@gmail.com>
2026-03-10 21:03:07 +0300

    some change

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,10 +1,10 @@
 import { IAuthForm } from "@/types/models";
 
 export function login(data: IAuthForm) {
-    return fetch(`${import.meta.env.VITE_BASE_URL}/auth/login`, {
+    return fetch(`${import.meta.env.VITE_BASE_URL}/users`, {
         method: "POST",
-        headers: { "Content_Type": "application/json" },
+        // headers: { "Content_Type": "application/json" },
         body: JSON.stringify(data),
-        credentials: "include"
+        // credentials: "include"
     })
 }
